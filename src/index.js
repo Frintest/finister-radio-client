2b284cfb206877a8a4c4d036b4813afe4eba8cb4
Dmitry <dimaruzaev2@gmai.com>
2024-08-03 16:14:10 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const api = "https://localhost:3002/song";
-const api = "https://airmonitor.servermc.ru:3002/song";
+// const api = "http://localhost:3002/song";
+const api = "https://finister-radio.servermc.ru:3002/song";
 
 const response = await axios.post(api);
 const songUrl = response.data;
